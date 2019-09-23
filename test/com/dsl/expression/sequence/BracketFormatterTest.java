bf58cf5592b7c0a0934aeff11674da61f4815a5e
dslsyeoh <syeoh91x@gmail.com>
2019-09-24 02:12:55 +0800

    add unit tests for Bracket Formatter

---
+++ test/com/dsl/expression/sequence/BracketFormatterTest.java
@@ -0,0 +1,105 @@
+/*
+ * Author Steven Yeoh
+ * Copyright (c) 2019. All rights reserved
+ */
+
+package com.dsl.expression.sequence;
+
+import com.dsl.expression.sequence.utils.BracketFormatter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+
+@TestInstance(PER_CLASS)
+public class BracketFormatterTest
+{
+    private BracketFormatter bracketFormatter;
+
+    @BeforeAll
+    public void setup()
+    {
+        bracketFormatter = new BracketFormatter();
+    }
+
+    @Test
+    public void testFormalizeToBracket_1()
+    {
+        Assertions.assertEquals("1+2-3+(1+3)", bracketFormatter.formalizeToBracket("1 + 2 - 3 + (1 + 3) == 6"));
+    }
+
+    @Test
+    public void testFormalizeToBracket_2()
+    {
+        Assertions.assertEquals("(3*2)+(1+2)", bracketFormatter.formalizeToBracket("3 * 2 + (1 + 2) == 6"));
+    }
+
+    @Test
+    public void testFormalizeToBracket_3()
+    {
+        Assertions.assertEquals( "(3*2)+1+2", bracketFormatter.formalizeToBracket("3 * 2 + 1 + 2 == 6"));
+    }
+
+    @Test
+    public void testFormalizeToBracket_4()
+    {
+        Assertions.assertEquals( "1+2+(2*3)", bracketFormatter.formalizeToBracket("1 + 2 + 2 * 3 == 6"));
+    }
+
+    @Test
+    public void testFormalizeToBracket_5()
+    {
+        Assertions.assertEquals( "(2*3)+(2*3)", bracketFormatter.formalizeToBracket("2 * 3 + 2 * 3 == 6"));
+    }
+
+    @Test
+    public void testFormalizeToBracket_6()
+    {
+        Assertions.assertEquals( "1+1+3+(2*3)+(2*3)", bracketFormatter.formalizeToBracket("1 + 1 + 3 + 2 * 3 + 2 * 3 == 6"));
+    }
+
+    @Test
+    public void testFormalizeToBracket_7()
+    {
+        Assertions.assertEquals("(2*3)+(2*3)+(2*3)+(2*3)+(2*3)+(2*3)", bracketFormatter.formalizeToBracket("2 * 3 + 2 * 3 + 2 * 3  + 2 * 3 + 2 * 3 + 2 * 3 == 6"));
+    }
+
+    @Test
+    public void testFormalizeToBracket_8()
+    {
+        Assertions.assertEquals( "(1/1)+3+(2*3)+(2*3)", bracketFormatter.formalizeToBracket("1 / 1 + 3 + 2 * 3 + 2 * 3 == 6"));
+    }
+
+    @Test
+    public void testFormalizeToBracket_9()
+    {
+        Assertions.assertEquals("(1/2/3/4*5*6)+7+8+9", bracketFormatter.formalizeToBracket("1 / 2 / 3 / 4 * 5 * 6 + 7 + 8 + 9 == 6"));
+    }
+
+    @Test
+    public void testFormalizeToBracket_10()
+    {
+        Assertions.assertEquals("1-2-(3*4)+(5/6)-(7*8)+9", bracketFormatter.formalizeToBracket("1 - 2 - 3 * 4 + 5 / 6 - 7 * 8 + 9 == 6"));
+    }
+
+    @Test
+    public void testFormalizeToBracket_11()
+    {
+        Assertions.assertEquals("(3*4/5)+5", bracketFormatter.formalizeToBracket("3 * 4 / 5 + 5 = 6"));
+    }
+
+    @Test
+    public void testFormalizeToBracket_12()
+    {
+        Assertions.assertEquals("(3*4/5/6)+5", bracketFormatter.formalizeToBracket("3 * 4 / 5 / 6 + 5 = 6"));
+    }
+
+    @Test
+    public void testFormalizeToBracket_13()
+    {
+        Assertions.assertEquals("(1/2/3/4*5*6)+7+(8*9)", bracketFormatter.formalizeToBracket("1 / 2 / 3 / 4 * 5 * 6 + 7 + 8 * 9"));
+    }
+
+}
